4e78eecc5a8791be27cb2fdfbf0f7b9dbeaed62e
ihorfastnakht <fastnakht.corp@gmail.com>
2018-12-11 09:45:13 +0200

    Updated closeGoogleNotifiers script

--- scripts/closeGoogleNotifiers.js
+++ scripts/closeGoogleNotifiers.js
@@ -1,21 +1,19 @@
-﻿(function () {
+(function (){
     try {
         var topNotifierButton = document.getElementsByClassName('gb_xa gb_rd gb_vd');
         for (var k = 0; k < topNotifierButton.length; k++) {
             topNotifierButton[k].click();
         }
-        var topNotifier = document.querySelector('gb_id gb_jd gb_7c gb_od gb_g');
-        if (topNotifier) {
-            for (var k = 0; k < topNotifier.length; k++) {
-                topNotifier[k].style.display = 'none';
-            }
+        var topNotifier = document.getElementsByClassName('gb_id gb_jd gb_7c gb_od gb_g');
+        for (var s = 0; s < topNotifier.length; s++) {
+            topNotifier[s].style.display = 'none';
         }
-
-        var gkeep = document.getElementsByClassName('gb_rc gb_jb gb_qc gb_Nd');
+        
+        var gkeep = document.getElementsByClassName('gb_id gb_jd gb_7c gb_od gb_g');
         for (var i = 0; i < gkeep.length; i++) {
             gkeep[i].style.display = 'none';
         }
-        var gcalendar = document.getElementsByClassName('gb_fa gb_g');
+        var gcalendar = document.getElementsByClassName('gb_id gb_jd gb_7c gb_od gb_g');
         for (var j = 0; j < gcalendar.length; j++) {
             gcalendar[j].style.display = 'none';
         }
