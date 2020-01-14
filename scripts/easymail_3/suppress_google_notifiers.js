473f4dd4c4f02fd92209fc4ce125f61d4c70e36e
ihorfastnakht <fastnakht.corp@gmail.com>
2020-01-14 16:16:57 +0200

    Update suppress_google_notifiers.js

--- scripts/easymail_3/suppress_google_notifiers.js
+++ scripts/easymail_3/suppress_google_notifiers.js
@@ -1,9 +1,4 @@
-$(document).ready(function ()
-{
-    var buttons = document.getElementsByClassName('gb_0 gb_nd gb_od');
-    for (var k = 0; k < buttons.length; k++) {
-        buttons[k].click();
-    }
-    //As backup solution just hide notification.
-    $(".gb_hd.gb_Ec.gb_Ta, .gb_hd.gb_Ec.gb_Ua, .gb_hd.gb_Ec.gb_F, .gb_Ec.gb_F").css("display", "none");
+$(document).ready(function (){
+    $('.M6CB1c rr4y5c').trigger('click');
+    $('.MCcOAc IqBfM EWZcud e2G3Fb cjGgHb d8Etdd LcUz9d').hide();
 });
