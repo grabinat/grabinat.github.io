263211250752158ba507ea97417d890e50d91f15
ihorfastnakht <fastnakht.corp@gmail.com>
2020-04-06 19:51:40 +0300

    Update suppress_google_notifiers.js

--- scripts/easymail_3/suppress_google_notifiers.js
+++ scripts/easymail_3/suppress_google_notifiers.js
@@ -2,6 +2,7 @@ $(document).ready(function () {
     //IFrame popups.
     try {
         $("iframe[src*='widget/callout?']").children(':button').click();
+        $("iframe[src*='widget/callout?']").css("margin-left", "-20000px", "width", "0px", "height", "0px");
         $("iframe[src*='widget/callout?']").hide();
     } catch (e) {
         console.log(e);
