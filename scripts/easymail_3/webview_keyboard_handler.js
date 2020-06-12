41e921375a8ecd84c93be93a12d104f25760f99a
ihorfastnakht <fastnakht.corp@gmail.com>
2020-06-12 16:49:45 +0300

    Update webview_keyboard_handler.js

--- scripts/easymail_3/webview_keyboard_handler.js
+++ scripts/easymail_3/webview_keyboard_handler.js
@@ -7,8 +7,38 @@
             window.addEventListener("keydown", keydown, false);
         }
         function keydown(e) {
-            if (e.altKey && e.ctrlKey && !e.shiftKey && (e.keyCode >= 48 && e.keyCode <= 57 || e.keyCode >= 65 && e.keyCode <= 90 || e.keyCode > 96 && e.keyCode <= 105))
-            {
+            //text template shortcuts
+            if (e.altKey && e.ctrlKey && !e.shiftKey && (e.keyCode >= 48 && e.keyCode <= 57 || e.keyCode >= 65 && e.keyCode <= 90 || e.keyCode > 96 && e.keyCode <= 105)) {
+                var payload = {
+                    Ctrl: e.ctrlKey,
+                    Alt: e.altKey,
+                    Shift: e.shiftKey,
+                    KeyCode: e.keyCode,
+                    Key: e.keyCode
+                }
+                var res = {
+                    Type: "KeyboardEvent",
+                    Value: JSON.stringify(payload)
+                }
+                showMsg(JSON.stringify(res));
+            }
+            //account and service shortcuts
+            else if ((e.ctrlKey || e.altKey) && !e.shiftKey && (e.keyCode >= 49 && e.keyCode <= 57 || e.keyCode >= 97 && e.keyCode <= 105)) {
+                var payload = {
+                    Ctrl: e.ctrlKey,
+                    Alt: e.altKey,
+                    Shift: e.shiftKey,
+                    KeyCode: e.keyCode,
+                    Key: e.keyCode
+                }
+                var res = {
+                    Type: "KeyboardEvent",
+                    Value: JSON.stringify(payload)
+                }
+                showMsg(JSON.stringify(res));
+            }
+            //commands
+            else if ((e.ctrlKey && !e.altKey && !e.shiftKey) && (e.keyCode == 70 || e.keyCode == 74 || e.keyCode == 82)) {
                 var payload = {
                     Ctrl: e.ctrlKey,
                     Alt: e.altKey,
